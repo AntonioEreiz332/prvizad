5fbe4300d364420938da41cfdce0338b34b0e65b
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-06-07 08:35:35 +0200

    Add files via upload

---
+++ prvizadatak/prvizadatak/KlasaG.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prvizadatak
+{
+    class KlasaG
+    {
+        public void Plus10(int broj)
+        {
+            broj = broj + 10;
+        }
+        public void f50(int broj)
+        {
+            broj = 50;
+        }
+
+    }
+}
---
+++ prvizadatak/prvizadatak/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prvizadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaG objekt = new KlasaG;
+            int broj = 40,Plus10,F50;
+            Plus10 = objekt.Plus10(broj);
+            F50 = objekt.f50(broj);
+            Console.WriteLine("Prva metoda iznosi: " + Plus10);
+            Console.WriteLine("Druga metoda iznosi: " + F50);
+
+            Console.ReadKey();
+        }
+    }
+}
