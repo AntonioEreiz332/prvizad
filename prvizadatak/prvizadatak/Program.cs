cde5168e4f10121440f18cae7f5fe8d305777cfc
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-06-07 08:42:49 +0200

    Add files via upload

--- prvizadatak/prvizadatak/Program.cs
+++ prvizadatak/prvizadatak/Program.cs
@@ -10,12 +10,10 @@ namespace prvizadatak
     {
         static void Main(string[] args)
         {
-            KlasaG objekt = new KlasaG;
-            int broj = 40,Plus10,F50;
-            Plus10 = objekt.Plus10(broj);
-            F50 = objekt.f50(broj);
-            Console.WriteLine("Prva metoda iznosi: " + Plus10);
-            Console.WriteLine("Druga metoda iznosi: " + F50);
+            KlasaG objekt = new KlasaG();
+            int broj = 40;
+            Console.WriteLine("Prva metoda iznosi: " + objekt.Plus10(broj));
+            Console.WriteLine("Druga metoda iznosi: " + objekt.f50(broj));
 
             Console.ReadKey();
         }
